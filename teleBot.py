0348c08e1e4179587967f15bbbc3252647787d40
dayanidi <93707264+dayanid@users.noreply.github.com>
2023-06-23 01:13:56 +0530

    Update teleBot.py

--- teleBot.py
+++ teleBot.py
@@ -6,25 +6,30 @@ chat_id = "1221832086"
 #{'first_name': 'AFPLUAAS', 'username': 'AFPLUAASbot', 'supports_inline_queries': False, 'id': 5584328065, 'can_read_all_group_messages': False, 'can_join_groups': True, 'is_bot': True}
 
 def welcomeMessage(update: Update, context):
-    key = update.effective_chat.id - 1221822562
     first_name = update.effective_chat.first_name
     last_name = update.effective_chat.last_name
-    bot.send_message(chat_id="1221832086", text=f"New User for Send data to your self \n User Name {first_name } {last_name}\n User ID {update.effective_chat.id}")
+    bot.send_message(chat_id="1221832086", text=f"New User for Telechat Bot \n User Name {first_name } {last_name}\n User ID {update.effective_chat.id}")
     bot.send_message(
         chat_id=update.effective_chat.id,
         text=f"""
-        Hello {first_name}! 🎉\n
-        Welcome to our community.
-        We are delighted to have you here.\n
-        Your Key is {key}.\n
-        If you have any questions or need assistance, please don't hesitate to ask. 😊\n\n
-        Enjoy your time and have a wonderful day! 😎\n\n
-        To access additional features, please visit http://dayanidicode.github.io.\n\n
-        You can use your key to log in and send data yourself anywhere. 💻📲
-        We appreciate your support! 🙏
-        """
+    Hello {first_name}! 🎉
+    Welcome to our community!
+
+    Your Chat ID is {chat_id}.
+
+    If you need assistance or have any questions, feel free to ask. 😊
+    Enjoy your time and have a wonderful day! 😎
+
+    Sign up : https://telechatbot.github.io/?type=signup&chatid={chat_id}  to access additional features with a 4-digit PIN.
+
+    Set your PIN to login easily and securely. Just 4 digits are enough!
+
+    Remember your PIN for convenient logins. 💻📲
+
+    We appreciate your support! 🙏
+
+    """
     )
-    bot.send_message(chat_id=update.effective_chat.id,text=f"Your Key is {key}")
 
 def send_to_chat(update: Update, context):
     message = update.message.text
@@ -39,10 +44,9 @@ def send_to_chat(update: Update, context):
             reply_id = command[1].strip()
             reply_message = command[2]
 
-            if command_mode == "reply":
+            if command_mode.lower() == "reply":
                 context.bot.send_message(chat_id=reply_id, text=reply_message)
-                reply = f"Message received from {sender_name} (ID: {sender_id}):\n\n{message}"
-                context.bot.send_message(chat_id=chat_id, text=reply)
+                context.bot.send_message(chat_id=chat_id, text="send")
             else:
                 reply = f"Message received from {sender_name} (ID: {sender_id}):\n\n{message}"
                 context.bot.send_message(chat_id=chat_id, text=reply)
